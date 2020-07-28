77cb83c007da499971c65242742d82fe2152a267
mehedi609 <35611282+mehedi609@users.noreply.github.com>
2020-07-29 01:30:46 +0600

    Tag CRUD functionality done.

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -8,79 +8,94 @@ use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('admin.tag.index');
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $tags = Tag::latest()->get();
+    return view('admin.tag.index', compact('tags'));
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    return view('admin.tag.create');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param \Illuminate\Http\Request $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $this->validate($request, [
+      'tag_name' => 'required',
+    ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Tag $tag)
-    {
-        //
-    }
+    $tag = new Tag();
+    $tag->name = $request->tag_name;
+    $tag->slug = str_slug($request->tag_name);
+    $tag->save();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Tag $tag)
-    {
-        //
-    }
+    return redirect(route('admin.tag.index'))->with('successMsg', 'Tag created successfully');
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Tag $tag)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param \App\Tag $tag
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Tag $tag)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Tag $tag)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param \App\Tag $tag
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(Tag $tag)
+  {
+    return view('admin.tag.edit', compact('tag'));
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param \Illuminate\Http\Request $request
+   * @param \App\Tag $tag
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Tag $tag)
+  {
+    $tag->name = $request->tag_name;
+    $tag->slug = str_slug($request->name);
+    $tag->save();
+
+    return redirect()->route('admin.tag.index')->with('successMsg', 'Tag updated successfully');
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param \App\Tag $tag
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Tag $tag)
+  {
+    $tag->delete();
+    return redirect()->route('admin.tag.index')->with('successMsg', 'Tag deleted successfully');
+  }
 }
