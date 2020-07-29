db53815afa6a8048fcd2fc358d456ee17d84ac24
mehedi609 <35611282+mehedi609@users.noreply.github.com>
2020-07-29 11:29:43 +0600

    Add Category form created.

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.backend.app')
 
-@section('title', "Create Tag")
+@section('title', "Create Category")
 
 @section('content')
   <div class="container-fluid">
@@ -10,29 +10,50 @@
         <div class="card">
           <div class="header">
             <h2>
-              ADD NEW TAG
+              ADD NEW CATEGORY
             </h2>
 
             @if ($errors->any())
               @foreach ($errors->all() as $error)
-                  <div class="alert alert-danger">
-                    {{$error}}
-                  </div>
+                <div class="alert alert-danger">
+                  {{$error}}
+                </div>
               @endforeach
             @endif
 
           </div>
           <div class="body">
-            <form action="{{route('admin.tag.store')}}" method="POST">
+            <form
+              action="{{route('admin.category.store')}}"
+              method="POST"
+              enctype="multipart/form-data"
+            >
               @csrf
               <div class="form-group form-float">
                 <div class="form-line">
-                  <input type="text" id="tag_name" class="form-control" name="tag_name">
-                  <label class="form-label">Tag Name</label>
+                  <input
+                    type="text"
+                    id="category_name"
+                    class="form-control"
+                    name="category_name"
+                  >
+                  <label class="form-label">Category Name</label>
                 </div>
               </div>
 
-              <a href="{{route('admin.tag.index')}}" class="btn btn-danger m-t-15 m-r-10 waves-effect">
+              <div class="form-group">
+                <input
+                  type="file"
+                  id="category_image"
+                  class="form-control"
+                  name="category_image"
+                >
+              </div>
+
+              <a
+                href="{{route('admin.category.index')}}"
+                class="btn btn-danger m-t-15 m-r-10 waves-effect"
+              >
                 BACK
               </a>
               <button type="submit" class="btn btn-primary m-t-15 waves-effect">
